2295d249614c3b5366e68c1039146dfff603162a
Phan Gia Anh <phangiaanhlop99@gmail.com>
2021-06-13 16:01:53 +0700

    add detect

---
+++ detectDuplicateImage/AssignmentDetectMapper.java
@@ -0,0 +1,14 @@
+package detectduplicate; 
+import java.io.* ;
+
+import org.apache.hadoop.io.BytesWritable ;
+import org.apache.hadoop.io.Text ; 
+import org.apache.hadoop.mapreduce.Mapper ; 
+
+public class AssignmentDetectMapper extends Mapper <Text, BytesWritable , Text , Text> {
+		public void map(Text key , BytesWritable value , Context context)
+		throws IOException , InterruptedException {
+				Text hash = new Text(Skein512.hash(value.getBytes() , value.getLength()));
+				context.write(hash, key );
+			}
+	}
